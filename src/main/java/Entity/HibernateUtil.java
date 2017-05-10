d8c1c3dfeb683adaa137ad52969f13f9a7c848d1
KubaGut <jakub_gutkowski@wp.pl>
2017-05-10 19:59:37 +0200

    version 1.0

---
+++ src/main/java/Entity/HibernateUtil.java
@@ -0,0 +1,50 @@
+package Entity;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class HibernateUtil {
+
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void saveObjectToDataBase(UserEntity user) {
+        Session session = getSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.save(user);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import Entity.HibernateUtil;
+
+public class Main {
+
+    public static void main(final String[] args) throws Exception {
+
+        HibernateUtil relacja = new HibernateUtil();
+
+
+    }
+
+
+}
